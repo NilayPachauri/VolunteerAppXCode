995425803079fb401bde40db99677db575464426
NilayPachauri <pachauri.nilay@gmail.com>
2015-07-23 23:33:41 -0700

    Got Picker to Constrain
    but it screwed everything else up

---
+++ Volunteer-Connect/ScrollViewController.h
@@ -0,0 +1,15 @@
+//
+//  ScrollViewController.h
+//  Volunteer-Connect
+//
+//  Created by Nilay on 7/23/15.
+//  Copyright (c) 2015 Akash Singhal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ScrollViewController : UIViewController  {
+
+    IBOutlet UIScrollView *scroller;
+
+} @end
