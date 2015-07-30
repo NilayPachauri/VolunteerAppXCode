cabee3f85170087ea62bd2600b786b2af3e88596
akashsinghal <akashksinghal98@gmail.com>
2015-07-29 23:40:14 -0700

    csv parser

--- Volunteer-Connect/ViewController.m
+++ Volunteer-Connect/ViewController.m
@@ -16,6 +16,29 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    NSString *strPath = [[NSBundle mainBundle] pathForResource:@"VolData" ofType:@"csv"];
+    NSString *strFile = [NSString stringWithContentsOfFile:strPath encoding:NSUTF8StringEncoding error:nil];
+    if (!strFile) {
+        NSLog(@"Error reading file.");
+    }
+    NSArray* data = [[NSArray alloc] init];
+    
+    data = [strFile componentsSeparatedByString:@"/n"];
+    NSMutableArray *strArray = [NSMutableArray array]; // This is your 2D array
+    for(int i = 0; i < [data count]; i++)
+    {
+        NSArray *translation = [data[i] componentsSeparatedByString:@","];
+        // translation is assumed to be an array with two elements
+        [strArray addObject:translation];
+    }
+    id myArrayElement  = [strArray objectAtIndex:1];
+    // NSLog(@"%@", myArrayElement);
+    NSLog(@"hi");
+    
+    // this .csv file is seperated with new line character
+    // if .csv is seperated by comma use "," instesd of "\n"
+    
+
     // Do any additional setup after loading the view, typically from a nib.
 }
 
